2bb3aaefdb4ef042d3d172f2d5741c40578d6477
popy-jopy49 <70320407+popy-jopy49@users.noreply.github.com>
2024-06-26 10:40:24 +1200

    Added movement, and started kits

---
+++ 2081/Assets/Scripts/CameraController/Scripts/CharacterControllerFPS.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterControllerFPS : MonoBehaviour {
+
+    [SerializeField] private CharacterController controller;
+
+    private float speed = 12f;
+    [SerializeField] private float walkSpeed = 12f;
+    [SerializeField] private float sprintSpeed = 25f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 3f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    private float currentEnergy = 0f;
+    [SerializeField] private float maxEnergy = 20f;
+    [SerializeField] private float energyRegain = 0.1f;
+    [SerializeField] private float jumpEnergy = 3f;
+    [SerializeField] private float sprintEnergy = 0.2f;
+
+    private void OnEnable()
+    {
+        currentEnergy = maxEnergy;
+        speed = walkSpeed;
+        InputManager.MAIN.Character.Sprint.started += Sprint;
+        InputManager.MAIN.Character.Sprint.canceled += Walk;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.MAIN.Character.Sprint.started -= Sprint;
+        InputManager.MAIN.Character.Sprint.canceled -= Walk;
+    }
+
+    private void Sprint(InputAction.CallbackContext ctx) => speed = sprintSpeed;
+    private void Walk(InputAction.CallbackContext ctx) => speed = walkSpeed;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
+
+        if(isGrounded && velocity.y < 0)
+		{
+            velocity.y = -2f;
+		}
+
+        Vector2 walkInput = InputManager.MAIN.Character.Move.ReadValue<Vector2>().normalized;
+
+        Vector3 move = transform.right * walkInput.x + transform.forward * walkInput.y;
+
+		if (InputManager.MAIN.Character.Jump.triggered && isGrounded && currentEnergy >= jumpEnergy)
+		{
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            currentEnergy -= jumpEnergy;
+		}
+
+        controller.Move(speed * Time.deltaTime * move);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (speed == sprintSpeed && walkInput != Vector2.zero)
+            currentEnergy -= sprintEnergy;
+        else
+            currentEnergy += energyRegain;
+
+        currentEnergy = Mathf.Clamp(currentEnergy, 0, maxEnergy);
+
+        if (currentEnergy <= 0)
+            speed = walkSpeed;
+    }
+    
+}
---
+++ 2081/Assets/Scripts/CameraController/Scripts/MouseLook.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    public float mouseSensitivity = 1000f;
+    public Transform player;
+
+    private float xRotation = 0f;
+
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+    {
+        Vector2 mouseInput = InputManager.MAIN.Character.Camera.ReadValue<Vector2>();
+        float mouseX = mouseInput.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = mouseInput.y * mouseSensitivity * Time.deltaTime;
+        player.Rotate(Vector3.up * mouseX);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+    
+}
