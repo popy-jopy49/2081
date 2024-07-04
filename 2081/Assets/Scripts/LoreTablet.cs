66764e2eff1629a4ea6051a6037709aedb1643d3
popy-jopy49 <70320407+popy-jopy49@users.noreply.github.com>
2024-07-05 09:38:07 +1200

    Added lore and fixed interaction

--- 2081/Assets/Scripts/LoreTablet.cs
+++ 2081/Assets/Scripts/LoreTablet.cs
@@ -5,7 +5,7 @@ public class LoreTablet : MonoBehaviour, IInteractable
 
     public void OnInteract()
     {
-
+        print("The spacestation was taken over by AI. Get to the control room and press the conveniently placed self destruct button to destroy it and end its rein of terror!");
         Destroy(gameObject);
     }
 
