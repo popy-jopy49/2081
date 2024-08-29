fe4a986db7511e07c8f04136245a6c61b090e11b
codenoob69 <86938037+codenoob69@users.noreply.github.com>
2024-08-30 09:34:12 +1200

    Did stuff
    Hi im stuff

--- 2081/Assets/Scripts/EndLevel.cs
+++ 2081/Assets/Scripts/EndLevel.cs
@@ -4,16 +4,27 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(BoxCollider))]
 public class EndLevel : MonoBehaviour
 {
+	[SerializeField] private string SceneName;
 
-	private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
 	{
 		if (!other.CompareTag("Player"))
 			return;
 
 		// Load next level
-		if (SceneManager.GetSceneByBuildIndex(SceneManager.GetActiveScene().buildIndex + 1).IsValid())
+		//if (SceneManager.GetSceneByBuildIndex(SceneManager.GetActiveScene().buildIndex + 1).IsValid())
+		//{
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		//}
+		//else
+		//{
+			//Cursor.lockState = CursorLockMode.None;
+			//SceneManager.LoadScene(0);
+		//}
+
+		if (SceneName != null)
 		{
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			SceneManager.LoadScene(SceneName);
 		}
 		else
 		{
---
+++ 2081/Assets/Scripts/UI/PlayVideo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        
+    }
+}
